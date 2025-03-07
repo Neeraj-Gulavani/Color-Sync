ea3e2f53f6538de3bf50dc163dfb3b47f129e221
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-03-07 10:59:36 +0530

    Pause Menu + Auto Destroying previously Instantiated GameObjects/Levels

--- ColorSync/Assets/Scripts/GameManager.cs
+++ ColorSync/Assets/Scripts/GameManager.cs
@@ -7,13 +7,34 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;   
     public GameObject[] templates;
     public int pscore=0;
+    public bool paused;
+    public GameObject pauseMenu;
+    public GameObject prevTemp;
+    public GameObject prevPrevTemp;
     void Start() {
         instance = this;
     }
     public static void SpawnNextTemplate(Vector3 pos, GameObject prevTemplate) {
+        if (instance.prevPrevTemp != null) {
+            Destroy(instance.prevPrevTemp);
+        }
+         instance.prevPrevTemp = instance.prevTemp;
         GameObject toSpawn = instance.templates[Random.Range(0,instance.templates.Length)];
-        GameObject spawned = Instantiate(toSpawn,pos,Quaternion.identity);
+        instance.prevTemp = Instantiate(toSpawn,pos,Quaternion.identity);
         
+    }
+
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause() {
+        Time.timeScale = paused?1:0;
+            paused=!paused;
+            pauseMenu.SetActive(paused);
     }
 
 }
