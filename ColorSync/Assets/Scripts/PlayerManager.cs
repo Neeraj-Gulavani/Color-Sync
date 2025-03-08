47adec966801809eeccbcb2b7d3cddf9bbfc9a5d
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-03-08 10:45:51 +0530

    Setup a brief Game Design Document and added Camera Shake effect

--- ColorSync/Assets/Scripts/PlayerManager.cs
+++ ColorSync/Assets/Scripts/PlayerManager.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using Cinemachine;
 public class PlayerManager : MonoBehaviour
 {
     private Renderer r;
     public static string playerColour;
+    private CinemachineImpulseSource impulseSrc;
     // Start is called before the first frame update
     void Awake()
     {
         r = GetComponent<Renderer>();
         playerColour = r.material.name.Replace(" (Instance)", "").ToLower();
+        impulseSrc =GetComponent<CinemachineImpulseSource>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,7 @@ public class PlayerManager : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        
+        impulseSrc.GenerateImpulseWithForce(1f);
         ColorAssign scr = other.GetComponent<ColorAssign>();
         if (scr!=null) {
             string playerMatName = r.material.name.Replace(" (Instance)", "").ToLower();
@@ -35,8 +37,22 @@ public class PlayerManager : MonoBehaviour
                 GameManager.instance.pscore = PlayerMovement.score;
                 SceneManager.LoadScene("Lose");
             }
+        
+        Color c;
+        if (ColorUtility.TryParseHtmlString(objMatName, out c))
+            {
+                var main= scr.transform.Find("CollectVFX").GetComponent<ParticleSystem>().main;
+                main.startColor = c;
+            }
+            scr.transform.Find("CollectVFX").GetComponent<ParticleSystem>().Clear();
+        scr.transform.Find("CollectVFX").GetComponent<ParticleSystem>().Play();
         scr.gameObject.SetActive(false);
         }
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        impulseSrc.GenerateImpulseWithForce(1f);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Color-Sync
+
+## Game Overview
+
+Title: Color Sync
+Genre: Hypercasual
+Platform: Windows
+Engine: Unity (C#)
+Target Audience: Casual Windows Gamers
+
+## Gameplay Summary
+
+Color Sync is a fast-paced, infinite runner game where the player controls a rolling ball that automatically moves forward. The player must navigate left and right to avoid obstacles and collect points.
+
+## Core Mechanics
+
+### Movement:
+
+- Forward movement is automatic.
+- Player controls left and right movement using swipe or tilt input.
+
+### Obstacles:
+
+- Obstacles appear at increasing frequencies.
+- Hitting an obstacle destroys your momentum.
+
+### Scoring:
+
+- Points awarded based on distance traveled.
+
+## Objective:
+
+- Survive as long as possible without hitting a pickup with the incorrect color.
+
+## Level Design:
+
+- Obstacles are generated dynamically based on a given set of templates.
+
+## Controls:
+
+- Use the Arrow keys or 'A' and 'D' keys to move left and right.
+
+## Installation:
+
+- Just download the builds folder and run the "ColorSync.exe" file to play the game!
+
